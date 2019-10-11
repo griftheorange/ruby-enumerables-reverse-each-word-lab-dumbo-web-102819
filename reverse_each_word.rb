53175f196b76488e020a524c57958994cf9e7b19
Griffin Poole <griffinpoole5@gmail.com>
2019-10-11 18:30:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(string)
   words = string.split(' ')
   new_sentence = []
   words.each { |word| new_sentence.push(word.reverse)}
-  new_sentence.join(' ')
+  new_string = new_sentence.join(' ')
 end
