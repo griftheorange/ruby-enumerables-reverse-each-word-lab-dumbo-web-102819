bd9a2e0566fc04a3fbceb27d521e1b8810d901e8
Griffin Poole <griffinpoole5@gmail.com>
2019-10-11 18:33:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(string)
   words = string.split(' ')
-  new_sentence = []
-  words.each { |word| new_sentence.push(word.reverse)}
-  new_sentence.join(' ')
+  words.collect { |word| word.reverse}
 end
